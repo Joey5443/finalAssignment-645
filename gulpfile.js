6d819d6db835222d36ed181c8668028c3869b541
Joey5443 <77410060+Joey5443@users.noreply.github.com>
2021-03-13 10:30:54 -0800

    minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 
 let { src, dest, watch } = require("gulp");
 let sass = require("gulp-sass");
+let cleanCSS = require("gulp-clean-css");
 sass.compiler = require("node-sass");
 
 function css() {
   return src("src/*.scss")
     .pipe(sass())
+    .pipe(cleanCSS({ compatibility: "ie8" }))
     .pipe(dest("dist/css"));
 }
 
